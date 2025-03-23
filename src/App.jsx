481ac63d352afa6ede46aac85df596e65606cdf4
prema46-sp <shanmugaprema5@gmail.com>
2025-03-23 12:06:54 +0530

    deploy react to github

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Form from "./Form";
 import Results from "./Results";
 
 function App() {
   return (
+   
     <Routes>
       <Route path="/" element={<Form />} />
       <Route path="/results" element={<Results />} />
     </Routes>
+ 
   );
 }
 
